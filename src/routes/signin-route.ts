d1c9100dd49546382b1c927ae39ae16275744d3f
omarhosny206 <omarhosny102@gmail.com>
2023-02-03 16:59:34 +0200

    Added linkedin authentication

--- src/routes/signin-route.ts
+++ src/routes/signin-route.ts
@@ -3,15 +3,16 @@ import { Router } from 'express';
 import * as signinController from '../controllers/signin-controller';
 import * as githubAuthentication from '../middlewares/github-authentication';
 import * as googleAuthentication from '../middlewares/google-authentication';
+import * as linkedinAuthentication from '../middlewares/linkedin-authentication';
 import * as twitterAuthentication from '../middlewares/twitter-authentication';
 import * as validator from '../middlewares/validator';
 import signinSchema from '../validations/signin-schema';
 
 const router: Router = Router();
-
 router.post('/', validator.validate(signinSchema), signinController.signin);
 router.post('/google', googleAuthentication.authenticateByAccessToken);
 router.post('/github', githubAuthentication.authenticateByAccessToken);
 router.post('/twitter', twitterAuthentication.authenticateByAccessToken);
+router.post('/linkedin', linkedinAuthentication.authenticateByAccessToken);
 
 export default router;
