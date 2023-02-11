c4613b50a2ae0ab2e74c4387729a1a7b07c26f17
omarhosny206 <omarhosny102@gmail.com>
2023-02-11 23:01:49 +0200

    Updated authorization-middleware by removing useless extra db call

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -1,22 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
-import { Types } from 'mongoose';
 
 import { Role } from '../enums/role-enum';
-import IUser from '../interfaces/users/user-interface';
 import * as roleService from '../services/role-service';
-import * as userService from '../services/user-service';
 import ApiError from '../utils/api-error';
 
 export function authorizeByRole(...allowedRoles: Role[]) {
   return async function (req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const _id: Types.ObjectId = req.authenticatedUser._id as Types.ObjectId;
-      const user: IUser | null = await userService.getById(_id);
-
-      if (!user) {
-        throw ApiError.unauthorized('Unauthorized: user not found');
-      }
-
+      const user = req.authenticatedUser;
       const isAuthorized: boolean = roleService.check(user.role, allowedRoles);
 
       if (!isAuthorized) {
