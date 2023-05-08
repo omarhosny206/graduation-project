153be2194b7bee3eea266fb95a9a9caaa8a5d719
omarhosny206 <omarhosny102@gmail.com>
2023-05-09 01:18:54 +0300

    Refactored image service

--- src/services/image-service.ts
+++ src/services/image-service.ts
@@ -1,23 +1,11 @@
-import { DeleteObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import crypto from 'crypto';
 import sharp from 'sharp';
 
 import ApiError from '../utils/api-error';
 import { AuthenticatedUser } from '../utils/authenticated-user-type';
+import * as awsS3Service from './aws-s3-service';
 
-const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY!!;
-const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!!;
-const AWS_BUCKET = process.env.AWS_BUCKET!!;
-const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION!!;
 const AWS_BUCKET_PUBLIC_URL = process.env.AWS_BUCKET_PUBLIC_URL!!;
-
-const s3Client = new S3Client({
-  region: AWS_BUCKET_REGION,
-  credentials: {
-    accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  },
-});
 
 export async function upload(user: AuthenticatedUser, fileBuffer: Buffer, contentType: string) {
   try {
@@ -31,15 +19,7 @@ export async function upload(user: AuthenticatedUser, fileBuffer: Buffer, conten
 
     fileBuffer = await sharpen(fileBuffer);
 
-    const uploadParams = {
-      Bucket: AWS_BUCKET,
-      Body: fileBuffer,
-      Key: fileKey,
-      ContentType: contentType,
-    };
-
-    const putObjectCommand = new PutObjectCommand(uploadParams);
-    await s3Client.send(putObjectCommand);
+    await awsS3Service.upload(fileKey, fileBuffer, contentType);
 
     if (user.imageKey !== fileKey) {
       user.imageKey = fileKey;
@@ -62,13 +42,7 @@ export async function remove(user: AuthenticatedUser) {
 
     const fileKey = user.imageKey;
 
-    const deleteParams = {
-      Bucket: AWS_BUCKET,
-      Key: fileKey,
-    };
-
-    const deleteCommand = new DeleteObjectCommand(deleteParams);
-    await s3Client.send(deleteCommand);
+    await awsS3Service.remove(fileKey);
 
     user.imageKey = '';
     user.imageUrl = '';
