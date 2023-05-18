c44aeda7bc1ae9011b05b89e5725908571ea18f6
omarhosny206 <omarhosny102@gmail.com>
2023-05-19 02:15:06 +0300

    Updated aws s3 config file

--- src/config/aws-s3-config.ts
+++ src/config/aws-s3-config.ts
@@ -2,9 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY!!;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!!;
-const AWS_BUCKET = process.env.AWS_BUCKET!!;
 const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION!!;
-const AWS_BUCKET_PUBLIC_URL = process.env.AWS_BUCKET_PUBLIC_URL!!;
 
 export const s3Client = new S3Client({
   region: AWS_BUCKET_REGION,
