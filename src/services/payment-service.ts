815a365ef735ddbac6680bd6f29ee5e4941da157
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-05-25 13:40:25 +0300

    refactored axios requests

--- src/services/payment-service.ts
+++ src/services/payment-service.ts
@@ -2,23 +2,22 @@ import axios from 'axios';
 import qs from 'qs';
 import ApiError from '../utils/api-error';
 
+const baseURL = {
+  sandbox: 'https://api-m.sandbox.paypal.com',
+  production: 'https://api-m.paypal.com',
+};
+
 export async function generateAccessToken() {
   try {
     const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID;
     const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
-    const { data } = await axios.post(
-      'https://api-m.sandbox.paypal.com/v1/oauth2/token',
-      qs.stringify({ grant_type: 'client_credentials' }),
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-        auth: {
-          username: PAYPAL_CLIENT_ID!,
-          password: PAYPAL_SECRET!,
-        },
-      }
-    );
+
+    const url = `${baseURL.sandbox}/v1/oauth2/token`;
+    const body = qs.stringify({ grant_type: 'client_credentials' });
+    const headers = { Accept: 'application/json' };
+    const auth = { username: PAYPAL_CLIENT_ID!, password: PAYPAL_SECRET! };
+
+    const { data } = await axios.post(url, body, { headers: headers, auth: auth });
     const accessToken = data.access_token;
     return accessToken;
   } catch (error) {
