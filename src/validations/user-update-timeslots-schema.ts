75ce08020e67c355f786d8b8ec399f907b0edaa9
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-06-18 21:59:13 +0300

    Updated timeslots schema making hours required

--- src/validations/user-update-timeslots-schema.ts
+++ src/validations/user-update-timeslots-schema.ts
@@ -10,7 +10,8 @@ const userUpdateTimeslotsSchema: Joi.ObjectSchema<IUserInfo> = Joi.object<IUserI
         day: Joi.number().valid(0, 1, 2, 3, 4, 5, 6).required(),
         hours: Joi.array()
           .items(Joi.string().regex(/^(0[0-9]|1[0-9]|2[0-3]):00|30$/))
-          .default([]),
+          .default([])
+          .required(),
       })
     )
     .min(7)
