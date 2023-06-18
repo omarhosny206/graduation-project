97ea97d7d8c14b92eca0bad8d36c749534d71d45
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-06-18 20:41:15 +0300

    Updated user-update-socials-schema

--- src/validations/user-update-socials-schema.ts
+++ src/validations/user-update-socials-schema.ts
@@ -4,7 +4,11 @@ import ISocials from '../interfaces/users/socials-interface';
 import IUserInfo from '../interfaces/users/user-info-interface';
 
 const userUpdateSocialsSchema: Joi.ObjectSchema<IUserInfo> = Joi.object<IUserInfo>({
-  socials: Joi.object<ISocials>({ github: Joi.string(), linkedin: Joi.string(), twitter: Joi.string() }).required(),
+  socials: Joi.object<ISocials>({
+    github: Joi.string().required(),
+    linkedin: Joi.string().required(),
+    twitter: Joi.string().required(),
+  }).required(),
 });
 
 export default userUpdateSocialsSchema;
