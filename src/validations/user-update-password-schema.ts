27a372bf9ac977f02220e434f6c9d601f8ca1294
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-06-24 12:39:17 +0300

    Updated password validation using regex

--- src/validations/user-update-password-schema.ts
+++ src/validations/user-update-password-schema.ts
@@ -2,10 +2,17 @@ import Joi from 'joi';
 
 import IPasswordUpdate from '../interfaces/users/password-update-interface';
 
+const passwordValidation = Joi.string()
+  .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+  .message(
+    '"{#label}": Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character'
+  )
+  .required();
+
 const userUpdatePasswordSchema: Joi.ObjectSchema<IPasswordUpdate> = Joi.object<IPasswordUpdate>({
-  oldPassword: Joi.string().min(8).required(),
-  newPassword: Joi.string().min(8).required(),
-  confirmPassword: Joi.string().min(8).required(),
+  oldPassword: passwordValidation.label('old password'),
+  newPassword: passwordValidation.label('new password'),
+  confirmPassword: passwordValidation.label('confirm password'),
 });
 
 export default userUpdatePasswordSchema;
