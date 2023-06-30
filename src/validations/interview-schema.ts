59c51aeb5f8bd11755e8df7b9901ce63e52d712f
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-06-30 14:02:12 +0300

    Updated objectid validation in interview-schema

--- src/validations/interview-schema.ts
+++ src/validations/interview-schema.ts
@@ -3,8 +3,8 @@ import Joi from 'joi';
 import IInterview from '../interfaces/interviews/interview-interface';
 
 const interviewSchema: Joi.ObjectSchema<IInterview> = Joi.object<IInterview>({
-  interviewee: Joi.string().required(),
-  interviewer: Joi.string().required(),
+  interviewee: Joi.string().regex(/^[0-9a-fA-F]{24}$/).message("interviewee is not a valid objectid").required(),
+  interviewer: Joi.string().regex(/^[0-9a-fA-F]{24}$/).message("interviewer is not a valid objectid").required(),
   date: Joi.date().greater('now').required(),
 });
 
