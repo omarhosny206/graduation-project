8dd982eaa0aa6f2142f6a329aa8fd7681dc96277
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-06-30 14:04:15 +0300

    Updated objectid validation in interview-update-review-schema

--- src/validations/interview-update-review-schema.ts
+++ src/validations/interview-update-review-schema.ts
@@ -3,8 +3,8 @@ import Joi from 'joi';
 import IReview from '../interfaces/interviews/review-interface';
 
 const interviewUpdateReviewSchema: Joi.ObjectSchema<IReview> = Joi.object<IReview>({
-  to: Joi.string().required(),
-  from: Joi.string().required(),
+  to: Joi.string().regex(/^[0-9a-fA-F]{24}$/).message("to is not a valid objectid").required(),
+  from: Joi.string().regex(/^[0-9a-fA-F]{24}$/).message("from is not a valid objectid").required(),
   feedback: Joi.string().required(),
   rating: Joi.number().min(0).max(5).required(),
 });
