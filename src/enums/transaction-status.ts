fdf5d4c21732db21bb13b24bbae5341d0d08504b
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-07-04 15:01:28 +0300

    transaction status & transaction service

---
+++ src/enums/transaction-status.ts
@@ -0,0 +1,9 @@
+export enum TransactionStatus {
+    Created = 'CREATED',
+    Completed = 'COMPLETED',
+    Declined = 'DECLINED',
+    PartiallyRefunded = 'PARTIALLY_REFUNDED',
+    Pending = 'PENDING',
+    Refunded = 'REFUNDED',
+    Failed = 'FAILED'
+}
