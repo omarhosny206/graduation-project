b4de285cedfe335b7480d1f53ab8aa1fe79fc9b6
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-07-08 14:46:39 +0300

    Updated finish-onboarding-schema by creating the missing rule

--- src/validations/finish-onboarding-schema.ts
+++ src/validations/finish-onboarding-schema.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const finishOnboardingSchema = Joi.object({
-  merchantId: Joi.string().message("merchantId is not valid").required(),
+  merchantId: Joi.string().regex(/^[0-9a-fA-F]{24}$/).message("merchantId is not a valid objectid").required(),
 });
 
 export default finishOnboardingSchema;
