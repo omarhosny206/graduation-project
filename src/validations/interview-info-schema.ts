ce6eccb8e91ab4af695f1bab9c0a43e891635618
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-07-09 03:21:11 +0300

    Updated interview-info-schema

--- src/validations/interview-info-schema.ts
+++ src/validations/interview-info-schema.ts
@@ -1,13 +1,12 @@
 import Joi from 'joi';
 
 import IInterviewInfo from '../interfaces/interviews/interview-info-interface';
-import IReview from '../interfaces/interviews/review-interface';
 
 const interviewInfoSchema: Joi.ObjectSchema<IInterviewInfo> = Joi.object<IInterviewInfo>({
   title: Joi.string().required(),
   summary: Joi.string().required(),
   tags: Joi.array().items(Joi.string().required()).required(),
-  youtubeUrl: Joi.string(),
+  youtubeUrl: Joi.string().allow("").required(),
 });
 
 export default interviewInfoSchema;
