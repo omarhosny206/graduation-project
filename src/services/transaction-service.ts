ca27512f0959d54ee435476e1138f9620e3c1afa
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-07-09 11:18:45 +0300

    Added meetingUrl creating after transaction being saved

--- src/services/transaction-service.ts
+++ src/services/transaction-service.ts
@@ -1,10 +1,19 @@
+import { Types } from 'mongoose';
+
 import ITransaction from '../interfaces/transactions/transaction-interface';
 import TransactionModel from '../models/transaction-model';
+import * as interviewService from '../services/interview-service';
 import ApiError from '../utils/api-error';
 
 export async function save(transaction: ITransaction) {
   try {
     const transactionToBeSaved: ITransaction = await TransactionModel.create(transaction);
+
+    const interview = await interviewService.getById(transactionToBeSaved.interview as Types.ObjectId);
+    interview.isPaid = true;
+    await interview.save();
+    await interviewService.createMeetingUrl(interview);
+
     return transactionToBeSaved;
   } catch (error) {
     throw ApiError.from(error);
